108ffa6844d8d96967da191ae71c6f2e3997d754
dollarb1llz <xd0llarb1llzx@gmail.com>
2018-03-22 20:48:41 -0500

    first commit

---
+++ archive.php
@@ -0,0 +1,18 @@
+<?php include 'res/inc/header.php'; ?>
+
+<!--supporting-->
+<div class="color-box orange">
+  <div class="banner archive">
+    <h1>The Archives</h1>
+    <h2>Explore where we have been</h2>
+  </div>
+  <div class="container">
+    <div class="c"></div>
+  </div>
+</div>
+
+<footer>
+
+</footer>
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,40 @@
+<?php include 'res/inc/header.php'; ?>
+
+
+<!--Supporting Section -->
+<div class="color-box pitch-dark before">
+  <div class="banner space">
+    <h1>..to the journey</h1>
+    <h2>Webpage Menu</h2>
+  </div>
+  <div class="container bg">
+    <p>
+      <span>The first page you should know of is probably the most important to the webpage itself.</span>
+      <span>It's the origin of the whole page.</span>
+      <span>I call it "Ideas" because it's <a href="./index.html">a place for me to get my thoughts out.</a></span>
+      <span>I always start my adventures on the computer with writing down thoughts and ideas i've collected.</span>
+    </p>
+    <img src="./res/img/deep-thinker.gif" class="left" />
+    <p>
+      <span>The next page is sorta like a Daily Update page.</span>
+      <span>On this page I want to summarize my day basically.</span>
+      <span>I'm thinking I can put a <a href="./today.html">video of me just summarizing my day</a> and talking about all the things I accomplished or didn't.</span>
+      <span>I will also include some text, images and links to other pages around the webpage.</span>
+        <SPAN>BREAK</SPAN>
+      <span>There is another key aspect to the webpage that aids me in my everyday climb.</span>
+      <span>I call this the "Tomorrow page" because I get to <a href="./tomorrow.html">plan my day ahead of time with pictures cool fonts</a>.</span>
+      <span>This is a page I plan on updating everyday.</span>
+      <span>Apparently planning your day out gives you incentives to follow it.</span>
+        <SPAN>BREAK</SPAN>
+    </p>
+    <div class="c"></div>
+  </div>
+</div>
+
+
+<footer>
+
+</footer>
+
+</body>
+</html>
